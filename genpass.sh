a556037f9cc53ab46baa1d44ce924505c0985031
Alex <alejandro.izquierdo.b@gmail.com>
2017-09-20 15:17:54 +0200

    Update genpass.sh

--- genpass.sh
+++ genpass.sh
@@ -2,5 +2,17 @@
 
 # simple script for passwords generation
 # 12 charachters by default or number of first argument
+
+SPECIAL='!"#$%&()*+,-./:;<=>?@[\]^_`{|}~'
+if [[ $1 == "-a" ]]; then
+    ALL=true
+    shift 1
+fi
+
 CHAR=${1:-12}
-head /dev/urandom | base64 | tr -cd '[[:alnum:]]' |  head -c$CHAR; echo
+
+if [[ $ALL == "true" ]]; then
+    head /dev/urandom | tr -cd "[[:alnum:]$SPECIAL]" |  head -c$CHAR; echo
+else
+    head /dev/urandom | tr -cd '[[:alnum:]]' |  head -c$CHAR; echo
+fi
